923966804c0430ca81af01f8d007ccee32e4be44
ruth geraldine parraguirre garcia <rparraguirregarcia829@gmail.com>
2022-12-07 11:28:34 -0600

    Crud Firebase

---
+++ public/firebase.js
@@ -0,0 +1,54 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  onSnapshot,
+  addDoc,
+  deleteDoc,
+  doc,
+  getDoc,
+  updateDoc,
+} from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBdf0NThbypz2WYZ0OqILElul5Px3FG8kg",
+    authDomain: "friendlychat-5d3fc.firebaseapp.com",
+    projectId: "friendlychat-5d3fc",
+    storageBucket: "friendlychat-5d3fc.appspot.com",
+    messagingSenderId: "942214277888",
+    appId: "1:942214277888:web:e59fc44e8d525ad25121e5"
+  };
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
+
+/**
+ * Save a New Task in Firestore
+ * @param {string} title the title of the Task
+ * @param {string} description the description of the Task
+ */
+export const saveTask = (title, description) =>
+  addDoc(collection(db, "tasks"), { title, description });
+
+export const onGetTasks = (callback) =>
+  onSnapshot(collection(db, "tasks"), callback);
+
+/**
+ *
+ * @param {string} id Task ID
+ */
+export const deleteTask = (id) => deleteDoc(doc(db, "tasks", id));
+
+export const getTask = (id) => getDoc(doc(db, "tasks", id));
+
+export const updateTask = (id, newFields) =>
+  updateDoc(doc(db, "tasks", id), newFields);
+
+export const getTasks = () => getDocs(collection(db, "tasks"));
